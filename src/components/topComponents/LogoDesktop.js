ec74c1388c7e68efa7fd67679940a0b919f705f7
Matheus Galdino <matheus@respondeai.com.br>
2021-08-24 18:42:53 -0300

    preparando o review

--- src/components/topComponents/LogoDesktop.js
+++
@@ -1,9 +0,0 @@
-export default function LogoDesktop() {
-    
-    return (
-        <div class="logo">
-            <a href="#/"><ion-icon name="logo-instagram" class="icon-instagram"></ion-icon></a>
-            <a href="#/"><img src="./imagens/logo.png" class="icon-logo" alt="logo"/></a>
-        </div>
-    );
-}
